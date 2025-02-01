87e20ceb20301cba043b9b6d9652a95a4d1ac84a
Pelin Ceylan <pelinceylan519@gmail.com>
2025-02-02 01:14:43 +0300

    Ödevler tamamlandı.

---
+++ cohorts-hafta8-odev1.js
@@ -0,0 +1,7 @@
+let yas = 18;
+
+if (yas >= 18) {
+    console.log("Ehliyet alabilirsiniz.");
+} else {
+    console.log("Ehliyet almak için yaşınız yeterli değil.");
+}
---
+++ cohorts-hafta8-odev2.js
@@ -0,0 +1,7 @@
+let sayi1 = 10;
+let sayi2 = 5;
+let toplam = sayi1 + sayi2;
+let carpma = sayi1 * sayi2;
+
+console.log("Toplam: " + toplam);
+console.log("Çarpma: " + carpma);
